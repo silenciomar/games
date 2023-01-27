60bca8659d0763f5b0b8f7da61ba8f31c6767f4f
silenciomar <96017754+silenciomar@users.noreply.github.com>
2023-01-27 14:57:59 +0600

    try to use shake function

--- magicBall8/script.js
+++ magicBall8/script.js
@@ -10,14 +10,23 @@ const phraseTruth = {
   5: 'Maybe'
 }
 
-ball.addEventListener('click', () => {
+let getTruth = () => {
   let randomNum = Math.trunc(Math.random() * 5) + 1;
 
   text.textContent = phraseTruth[randomNum];
-}) 
+}
 
-window.addEventListener('shake',  () => {
-  let randomNum = Math.trunc(Math.random() * 5) + 1;
+ball.addEventListener('click', getTruth)
 
-  text.textContent = phraseTruth[randomNum];
-})
+var myShakeEvent = new Shake({
+  threshold: 15, // optional shake strength threshold
+  timeout: 1000 // optional, determines the frequency of event generation
+});
+
+myShakeEvent.start();
+
+window.addEventListener('shake', getTruth, false)
+
+window.removeEventListener('shake', getTruth, false)
+
+myShakeEvent.stop();
